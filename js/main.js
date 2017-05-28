9b9bfe05a8a6f5dd5695b20ca7c04d064ca507c4
bovanher <bovanher@student.arteveldehs.be>
2017-05-28 15:26:23 +0200

    webd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-;(function() {
+/*;(function() {
   alert('Welcome to the GDM Web Template developed by drdynscript!');
-})();
+})();*/
+
+function myFunction() {
+    alert("Geweldig! Je ontvangt binnenkort onze nieuwsbrief!");
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
+    return i;
+}
